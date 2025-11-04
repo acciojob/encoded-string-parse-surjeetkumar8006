5dff3fb37510a454a41151dcc24b33529130dd5f
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2025-11-04 11:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
-  // your code here
+  // Split the string by one or more zeros using regex
+  const parts = str.split(/0+/);
+  
+  // Return the object with required keys
+  return {
+    firstName: parts[0],
+    lastName: parts[1],
+    id: parts[2]
+  };
 };
 
 // Do not change the code below
